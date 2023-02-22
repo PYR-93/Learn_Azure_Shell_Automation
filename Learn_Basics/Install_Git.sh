fa4ed5f46daac300055f0bf93feafbfa90639878
Yogesh_Reddy <yogeshwarreddyp@gmail.com>
2023-02-22 12:14:41 +0530

    added new files for git

---
+++ Learn_Basics/Install_Git.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+yum install git
+
+git clone https://github.com/PYR-93/Learn_Azure_Shell_Automation.git
--- Learn_Basics/She_Bang.sh
+++ Learn_Basics/She_Bang.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+yum install git
+
+git clone https://github.com/PYR-93/Learn_Azure_Shell_Automation.git
+
 hostname
 ls 
