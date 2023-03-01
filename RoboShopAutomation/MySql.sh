f67e02ef159bcef432c6c546ae3878de833d1e7b
Yogesh_Reddy <yogeshwarreddyp@gmail.com>
2023-03-02 03:25:10 +0530

    update

--- RoboShopAutomation/MySql.sh
+++ RoboShopAutomation/MySql.sh
@@ -24,15 +24,16 @@ systemctl enable mysqld &>>${Logfile}
 StatusCheck $?
 systemctl start mysqld &>>${Logfile}
 StatusCheck $?
-
-grep root@localhost: /var/log/mysqld.log | cut -d ' ' -f 11 >> tempPass.txt 
+echo "*****************************fetching the password from the log file -------------------"
+grep root@localhost: /var/log/mysqld.log | cut -d ' ' -f 11 >> tempPass.txt &>>${Logfile}
+StatusCheck $?
 last_entry=$(tail -n 1 tempPass.txt)
 # echo $last_entry
 
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$1';
-FLUSH PRIVILEGES;" > /tmp/pass.txt &>>${Logfile}
+echo " ALTER USER 'root'@'localhost' IDENTIFIED BY '$1';
+FLUSH PRIVILEGES; " > /tmp/pass.txt
 StatusCheck $?
-
+echo "*******Entering the validation for the default*********"
 echo "show databases;" | mysql -uroot -p${last_entry} &>>${Logfile}
 if [ $? -eq 0 ]; then
     echo "----Changing the password--------"
