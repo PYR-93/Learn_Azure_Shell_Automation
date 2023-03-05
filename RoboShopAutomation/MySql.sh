6b7210604b6872570ff4376b9f48df57abf92644
Yogesh_Reddy <yogeshwarreddyp@gmail.com>
2023-03-05 22:36:30 +0530

    update

--- RoboShopAutomation/MySql.sh
+++ RoboShopAutomation/MySql.sh
@@ -30,14 +30,14 @@ StatusCheck $?
 last_entry=$(tail -n 1 tempPass.txt)
 # echo $last_entry
 
-echo " ALTER USER 'root'@'localhost' IDENTIFIED BY '$1';
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$1';
 FLUSH PRIVILEGES; " > /tmp/pass.txt
 StatusCheck $?
 echo "*******Entering the validation for the default*********"
-echo "show databases;" | mysql -uroot -p${last_entry} &>>${Logfile}
+echo "show databases;" | mysql --connect-expired-password -uroot -p${last_entry} &>>${Logfile}
 if [ $? -eq 0 ]; then
     echo "----Changing the password--------"
-    mysql -uroot -p${last_entry} < /tmp/pass.txt &>>${Logfile}
+    mysql --connect-expired-password -uroot -p${last_entry} < /tmp/pass.txt &>>${Logfile}
     StatusCheck $?
     echo " uninstall plugin validate_password; " | mysql -uroot -p$1 &>>${Logfile}
     StatusCheck $?
