d2448ba7e307738e84c5d0ae5d8695ab5735e923
Yogesh_Reddy <yogeshwarreddyp@gmail.com>
2023-03-05 16:50:26 +0530

    up

--- RoboShopAutomation/VM-Creation.sh
+++ RoboShopAutomation/VM-Creation.sh
@@ -1,18 +1,21 @@
-vmname="myVM"
-resourcegroup="Learn_Azure_Focus"
-username="centosvm1"
-password="@BeStrong4816"
-az vm create \
-    --resource-group $resourcegroup \
-    --name $vmname \
-    --image eurolinuxspzoo1620639373013:centos-8-5-free:centos-8-5-free:8.5.5 \
-    --admin-username $username \
-    --admin-password $password \
-    --size Standard_B1s \
-    --public-ip-address "" \
-    --vnet-name Learn_Azure_Focus-vnet \
-    --subnet default
+for component in user cart catalogue payment shipping frontend mongodb redis mysql rabbitmq dispatch; do
+    echo "---Create $component VM ----
+
+    vmname=$component
+    resourcegroup="Learn_Azure_Focus"
+    username="centosvm1"
+    password="@BeStrong4816"
+    az vm create \
+        --resource-group $resourcegroup \
+        --name $vmname \
+        --image eurolinuxspzoo1620639373013:centos-8-5-free:centos-8-5-free:8.5.5 \
+        --admin-username $username \
+        --admin-password $password \
+        --size Standard_B1s \
+        --public-ip-address "" \
+        --vnet-name Learn_Azure_Focus-vnet \
+        --subnet default
     
-   
+done   
 
 # az vm create --resource-group azure-training-2023 --name $component --image OpenLogic:CentOS-LVM:8-lvm-gen2:8.5.2022101401 --vnet-name azure-training-2023-vnet --subnet default  --admin-username centos --admin-password DevOps654321 --public-ip-address "" --size Standard_B1s --nsg "" 
