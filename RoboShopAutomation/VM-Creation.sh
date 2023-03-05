d28009bb6071a1f2fa783e81a37b1a6ab7771662
Yogesh_Reddy <yogeshwarreddyp@gmail.com>
2023-03-05 17:46:09 +0530

    update

--- RoboShopAutomation/VM-Creation.sh
+++ RoboShopAutomation/VM-Creation.sh
@@ -1,6 +1,5 @@
 for component in user cart catalogue payment shipping frontend mongodb redis mysql rabbitmq dispatch; do
     echo "---Create $component VM ----
-
     vmname=$component
     resourcegroup="Learn_Azure_Focus"
     username="centosvm1"
@@ -14,8 +13,6 @@ for component in user cart catalogue payment shipping frontend mongodb redis mys
         --size Standard_B1s \
         --public-ip-address "" \
         --vnet-name Learn_Azure_Focus-vnet \
-        --subnet default
-    
+         --subnet default \
+         --nsg none
 done   
-
-# az vm create --resource-group azure-training-2023 --name $component --image OpenLogic:CentOS-LVM:8-lvm-gen2:8.5.2022101401 --vnet-name azure-training-2023-vnet --subnet default  --admin-username centos --admin-password DevOps654321 --public-ip-address "" --size Standard_B1s --nsg "" 
