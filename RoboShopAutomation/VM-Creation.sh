a50d619784cef7218fefd2f4771bade85ed78db5
Yogesh_Reddy <yogeshwarreddyp@gmail.com>
2023-03-05 16:12:14 +0530

    up

--- RoboShopAutomation/VM-Creation.sh
+++ RoboShopAutomation/VM-Creation.sh
@@ -5,12 +5,12 @@ password="@BeStrong4816"
 az vm create \
     --resource-group $resourcegroup \
     --name $vmname \
-    --image Win2022AzureEditionCore \
+    --image eurolinuxspzoo1620639373013:centos-8-5-free:centos-8-5-free:8.5.5 \
     --admin-username $username \
     --admin-password $password \
     --size StandardB1s \
     --public-ip-address "" \
-    --image eurolinuxspzoo1620639373013:centos-8-5-free:centos-8-5-free:8.5.5 \
+    
    
 
 # az vm create --resource-group azure-training-2023 --name $component --image OpenLogic:CentOS-LVM:8-lvm-gen2:8.5.2022101401 --vnet-name azure-training-2023-vnet --subnet default  --admin-username centos --admin-password DevOps654321 --public-ip-address "" --size Standard_B1s --nsg "" 
